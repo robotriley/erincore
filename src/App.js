410a86bb8b2267d4341c055aa9e6344d7d50d080
riley soloner <rileysoloner@gmail.com>
2022-12-10 09:59:40 -0800

    wireframing with words ,you know the deal.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <h1>ERINCORE</h1>
       <h3>90 MINUTES OR LESS IS BEST</h3>
+      <h2> here's where the definition of erincore movies will go</h2>
+      <h5>then a button that says something like reccomend me an erincore movie</h5>
+      <h6>then when you click the button, the movie poster and the movie info will go here. </h6>
     </div>
   );
 }
